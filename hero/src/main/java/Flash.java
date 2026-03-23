804a9f6f3493d8099df9a38b5328564349146085
samzz <samuelsanchez032007@gmail.com>
2026-03-22 23:06:26 -0400

    primer hero

---
+++ hero/src/main/java/Flash.java
@@ -0,0 +1,19 @@
+public class Flash extends hero {
+
+    @Override
+    protected void aGolpe() {
+        System.out.println("this hero");
+    } ;
+
+    @Override
+    protected void bStrategy() {
+        System.out.println("this hero");
+
+    };
+
+    @Override
+    protected void cSpeed() {
+        System.out.println("this hero");
+
+    };
+}
