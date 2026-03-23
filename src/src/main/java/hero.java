e4b1cbc7a65b2e8a6339c9af7230343af174c7a2
samzz <samuelsanchez032007@gmail.com>
2026-03-22 22:36:58 -0400

    primer commit

---
+++ src/src/main/java/hero.java
@@ -0,0 +1,13 @@
+public abstract class hero {
+   protected int numHero;
+   protected String nameHero;
+   protected double pesoHero;
+   protected String sexHero;
+   protected int tempHero;
+
+
+   protected abstract void aGolpe();
+   protected abstract void bStrategy();
+   protected abstract void cSpeed();
+
+}
